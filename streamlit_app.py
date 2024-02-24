253830d358b504e7d2319c47ed71717c5db3f6e3
Eliasmarcet91 <73188124+Eliasmarcet91@users.noreply.github.com>
2024-02-24 16:44:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import pickle
 
+
 # Define a function to load the model and apply the st.cache decorator
 @st.cache(allow_output_mutation=True)
 def load_model():
-    with open('nlp.pkl', 'rb') as file:
+    with open('model_cbow.pkl', 'rb') as file:
         model = pickle.load(file)
     return model
 
 # Load the pickled model using the cached function
 model = load_model()
+
 
 # Setting up the sidebar
 st.sidebar.title("Options")
@@ -19,15 +21,8 @@ st.sidebar.info("This NLP app uses a pre-trained model to check word2vec on the 
 st.title('Word2Vec')
 
 # User input in sidebar
-user_word = st.sidebar.text_input("Enter a word to get its vector:", "")
+user_input = st.sidebar.text_area("Enter Text for Analysis", "")
 
 # Main area for display output
-if st.sidebar.button('Get Word Vector'):
-    if user_word:
-        try:
-            word_vector = model.wv[user_word]  # Get the vector for the user input word
-            st.write(f"Vector for '{user_word}': {word_vector}")
-        except KeyError:
-            st.error(f"Word '{user_word}' not found in the vocabulary.")
-    else:
-        st.warning("Please enter a word.")
+if st.sidebar.button('Predict Sentiment'):
+    prediction = model.predict([user_input])
