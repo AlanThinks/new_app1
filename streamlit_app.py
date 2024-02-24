4ff1a39051f46169088cd81f94346a785fbebce5
Eliasmarcet91 <73188124+Eliasmarcet91@users.noreply.github.com>
2024-02-24 16:46:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ model = load_model()
 
 # Setting up the sidebar
 st.sidebar.title("Options")
-st.sidebar.info("This NLP app uses a pre-trained model to check word2vec on the script of American Psycho.")
+st.sidebar.info("This NLP app uses a pre-trained model to check word2vec on the script of American Psycho. For example type the word Bateman")
 
 # Main application
 st.title('Word2Vec')
