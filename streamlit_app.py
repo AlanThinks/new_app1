de1329efce3cf2d663b9daa3da7e554de618f64c
Eliasmarcet91 <73188124+Eliasmarcet91@users.noreply.github.com>
2024-02-25 10:50:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ os.system('pip install matplotlib')
 import streamlit as st
 import pickle
 import matplotlib.pyplot as plt
-
+from sklearn.decomposition import PCA
 
 # Define a function to load the model and apply the st.cache decorator
 @st.cache(allow_output_mutation=True)
@@ -15,18 +15,14 @@ def load_model():
 # Load the pickled model using the cached function
 model = load_model()
 
-
 # Setting up the sidebar
 st.sidebar.title("Options")
-st.sidebar.info("This NLP app uses a pre-trained model to check word2vec on the script of American Psycho. For example type the word Bateman")
+st.sidebar.info("This NLP app uses a pre-trained model to check word2vec on the script of American Psycho. For example, type the word Bateman")
 
 # Main application
 st.title('Word2Vec')
 
 # User input in sidebar
-#user_input = st.sidebar.text_area("Enter Text for Analysis", "")
-
-# Main area for display output
 user_word = st.sidebar.text_input("Enter a word to get its vector:", "")
 
 # Main area for display output
@@ -35,6 +31,22 @@ if st.sidebar.button('Get Word Vector'):
         try:
             word_vector = model.wv[user_word]  # Get the vector for the user input word
             st.write(f"Vector for '{user_word}': {word_vector}")
+
+            # Plot diagram
+            words = [user_word]
+            vectors = [model.wv[user_word]]
+
+            pca = PCA(n_components=2)
+            principal_components = pca.fit_transform(vectors)
+            principal_df = pd.DataFrame(data=principal_components, columns=['PC1', 'PC2'], index=words)
+
+            # Plot
+            fig, ax = plt.subplots()
+            ax.scatter(principal_df['PC1'], principal_df['PC2'])
+            ax.set_xlabel('PC1')
+            ax.set_ylabel('PC2')
+            ax.annotate(user_word, (principal_df['PC1'][0], principal_df['PC2'][0]))  # Annotate the user input word
+            st.pyplot(fig)
         except KeyError:
             st.error(f"Word '{user_word}' not found in the vocabulary.")
     else:
