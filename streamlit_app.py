2a4d95628e957f6244456a6fa70326810255b2db
Eliasmarcet91 <73188124+Eliasmarcet91@users.noreply.github.com>
2024-02-25 12:12:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ if user_input:
             ax.annotate(f"{word}\n{x[i]:.2f}, {y[i]:.2f}", (x[i], y[i]), textcoords="offset points", xytext=(5,5), ha='center')  # Annotate words with coordinates
         ax.set_xlabel('Dimension 1')
         ax.set_ylabel('Dimension 2')
-        ax.set_title(f"Word Vectors for '{user_input}'")
+        #ax.set_title(f"Word Vectors for '{user_input}'")
         ax.legend()
 
         # Adjusting the limits of x and y axes to ensure all points stay inside the plot
